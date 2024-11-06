e48f3d1a7d746eced67d2a0e2dc6a28d78276e21
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-11-06 13:06:22 -0600

    Pushing what I have for redis right now. Can not use redis command

--- utils/redis.js
+++ utils/redis.js
@@ -6,7 +6,7 @@ class RedisClient {
     client.on('error', err => console.log('Redis Constructor Client Error: ', err));
   }
 
-  async IsAlive(self) {
+  async isAlive(self) {
     // Method to test if redis is able to be connected to
     // Returns true on connection and false when cannot connect
 
@@ -24,7 +24,7 @@ class RedisClient {
   async get(self, key) {
     // Gets a value at a specific key from the cache
     if (typeof key !== 'string') {
-      // key variable if not a string
+      // key variable is not a string
       console.log('Redis get method key must be a string');
       return;
     }
@@ -32,6 +32,36 @@ class RedisClient {
     return value;
   }
 
+  async set(self, key, value, duration) {
+    // Set a value with a duration in cache
+    if (typeof key !== 'string') {
+      // key variable is not a string
+      console.log('Redis get method key must be a string');
+      return;
+    };
+
+    try {
+      await self.client.set(key, value, 'EX', duration);
+    } catch (err) {
+      console.log('Error setting key in Redis:', err);
+    }
+  }
+
+  async del(self, key) {
+    // Remove a value from the cache
+    if (typeof key !== 'string') {
+      console.log('Redis del method key must be a string');
+      return;
+    }
+
+    try {
+      await self.client.del(key);
+    } catch (err) {
+      console.log('Error deleting key in Redis:', err);
+    }
+  }
+
 }
 
-module.exports = RedisClient;
+const redisClient = new RedisClient();
+module.exports = redisClient;
