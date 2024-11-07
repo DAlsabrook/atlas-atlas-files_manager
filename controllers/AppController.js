7d9145366d94a4565d59b63399ff74aaabd6675d
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-11-07 11:39:54 -0600

    Pushing some logs

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,35 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-const getStatus = async () => {
-  const redisIsAlive = await redisClient.isAlive();
-  const dbIsAlive = await dbClient.isAlive();
-  return { 'redis': redisIsAlive, 'db': dbIsAlive }
+const getStatus = async (req, res) => {
+  try {
+    const redisIsAlive = await redisClient.isAlive();
+    
+    console.log("redisIsAlive status:");
+    console.log(redisIsAlive);
+
+    const dbIsAlive = await dbClient.isAlive();
+
+    console.log("dbIsAlive status:");
+    console.log(dbIsAlive);
+
+    res.status(200).send({ redis: redisIsAlive, db: dbIsAlive });
+  } catch (error) {
+    res.status(500).send({ error: 'An error occurred' });
+  }
 };
 
-const stats = async () => {
-  const numUsers = await dbClient.nbUsers();
-  const numFiles = await dbClient.nbfiles()
-  return { "users": numUsers, "files": numFiles }
-}
+const stats = async (req, res) => {
+  try {
+    const numUsers = await dbClient.nbUsers();
+    const numFiles = await dbClient.nbFiles();
+    res.status(200).send({ users: numUsers, files: numFiles });
+  } catch (error) {
+    res.status(500).send({ error: 'An error occurred' });
+  }
+};
 
 module.exports = {
   getStatus,
-  stats
-}
+  stats,
+};
