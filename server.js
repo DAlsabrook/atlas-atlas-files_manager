98c7e2ef983012fd843e8e11f001c81e991f4d7e
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-11-11 14:20:46 -0600

    Mongo Atlas

--- server.js
+++ server.js
@@ -13,3 +13,17 @@ app.listen(port, () => {
 })
 
 module.exports = app;
+
+
+// Start all services
+// service redis-server start
+// service mongodb start
+// npm run start-server
+
+
+// curl 0.0.0.0:5000/status ; echo ""
+
+// token : f9fff642-4591-4508-9e63-a866fdbb01bd
+// {"id":"67325d496cbd646b3b052343","email":"bob@dylan.com"}
+
+// curl -XPOST 0.0.0.0:5000/files -H "X-Token: 09c9a38a-b76c-46f5-9207-9298d53a8f82" -H "Content-Type: application/json" -d '{ "name": "myText.txt", "type": "file", "data": "SGVsbG8gV2Vic3RhY2shCg==" }' ; echo ""
